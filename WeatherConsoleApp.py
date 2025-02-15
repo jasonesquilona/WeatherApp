550f38501ef800c9672c217f646bf7dd6dac79a9
Jason <jasonresquilona@gmail.com>
2025-02-14 18:23:57 -0800

    Allowed for the viewing of temperature day by day instead of hour by hour

--- WeatherConsoleApp.py
+++ WeatherConsoleApp.py
@@ -25,8 +25,8 @@ def checkLongitude(long):
         return False   
     
 def getLongLat():
-    lat = float(input("Enter latitude"))
-    long = float(input("Enter longitude"))
+    lat = float(input("Enter latitude\n"))
+    long = float(input("Enter longitude\n"))
     if(checkLongitude(long) == False or checkLatitude(lat) == False):
         return
     else:
@@ -35,13 +35,13 @@ def getLongLat():
             "longitude": long,
             "hourly": "temperature_2m"
         }
-        response = CallWeatherAPI(params)
+        response = CallWeatherAPI.callApi(params)
         getTemperatureResponse(response)
         
 def getTemperatureResponse(response):
     hourly = response.Hourly()
     # https://pypi.org/project/openmeteo-requests/
-    hourly_temperature_2m = hourly.Variables(0).ValueasNumpy()
+    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
     
     hourly_data = {"date": pd.date_range(
         start = pd.to_datetime(hourly.Time(), unit = "s"),
@@ -51,7 +51,11 @@ def getTemperatureResponse(response):
     )}
     hourly_data["temperature_2m"] = hourly_temperature_2m
     hourly_dataframe_pd = pd.DataFrame(data = hourly_data)
-    print(hourly_dataframe_pd)
+    daily_dataframe_pd = hourly_dataframe_pd.copy(True)
+    daily_dataframe_pd['date'] = daily_dataframe_pd['date'].astype("datetime64[ns]").dt.date
+    daily_dataframe_pd = daily_dataframe_pd.set_index("date")
+    daily_dataframe_pd = daily_dataframe_pd.groupby(daily_dataframe_pd.index).agg(['min', 'max', 'mean'])
+    print(daily_dataframe_pd)
     
     
 def main():
@@ -67,3 +71,6 @@ def main():
                 selection = None
         except ValueError:
             print("Invalid choice. Enter a valid menu number")
+
+if __name__=="__main__":
+    main()
